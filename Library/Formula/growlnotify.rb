2fd7ec93664bcb259a8ca998f1afd2d4de50f6fe
Eero Saynatkari <projects@kittensoft.org>
2010-04-24 05:28:01 -0700

    Fixed growlnotify md5 after verifying file.

--- Library/Formula/growlnotify.rb
+++ Library/Formula/growlnotify.rb
@@ -1,10 +1,12 @@
-require 'formula'
+require "formula"
 
-class Growlnotify <Formula
-  version '1.2'
-  url 'http://github.com/indirect/growlnotify/tarball/v1.2'
-  md5 '9941d5c49862f5391877023fc3baec49'
-  homepage 'http://growl.info/extras.php#growlnotify'
+class Growlnotify < Formula
+  homepage  "http://growl.info/extras.php#growlnotify"
+
+  url       "http://github.com/indirect/growlnotify/tarball/v1.2"
+  version   "1.2"
+
+  md5       "b7f4436ab3916d3d1072467cecf7e4ee"
 
   def install
     bin.install "growlnotify"
